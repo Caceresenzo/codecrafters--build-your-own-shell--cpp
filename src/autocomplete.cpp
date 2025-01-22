2554b79e681ff3978db1aa39a8157f2c0ba00667
Caceresenzo <caceresenzo1502@gmail.com>
2025-01-22 02:44:58 +0100

    pass 6th autocompletion

--- src/autocomplete.cpp
+++ src/autocomplete.cpp
@@ -6,25 +6,28 @@
 #include <functional>
 #include <filesystem>
 
-struct string_comparator
-{
-    bool operator()(const std::string &x, const std::string &y) const
-    {
-        return (std::less<size_t>()(x.length(), y.length()) || std::less<std::string>()(x, y));
-    }
-};
-
-static void commit(std::string &line, const std::string &candidate)
-{
-    line += candidate;
-    std::cout << candidate << std::flush;
-
-    line += ' ';
-    std::cout << ' ' << std::flush;
-}
-
 namespace autocompletion
 {
+    struct string_comparator
+    {
+        bool operator()(const std::string &x, const std::string &y) const
+        {
+            return (std::less<size_t>()(x.length(), y.length()) || std::less<std::string>()(x, y));
+        }
+    };
+
+    static void commit(std::string &line, const std::string &candidate, bool has_more)
+    {
+        line += candidate;
+        std::cout << candidate << std::flush;
+
+        if (has_more)
+            return;
+
+        line += ' ';
+        std::cout << ' ' << std::flush;
+    }
+
     static void collect_builtins(std::set<std::string, string_comparator> &candidates, const std::string &line)
     {
         for (auto &builtin : builtins::REGISTRY)
@@ -70,6 +73,43 @@ namespace autocompletion
         }
     }
 
+    static std::optional<std::string> find_shared_prefix(const std::set<std::string, string_comparator> &candidates)
+    {
+        std::string first = *candidates.begin();
+        if (first.empty())
+            return (std::nullopt);
+
+        std::vector<std::string> others(std::next(candidates.begin()), candidates.end());
+
+        size_t end = 0;
+        for (; end <= first.size(); ++end)
+        {
+            bool one_is_not_matching = false;
+
+            for (const auto &other : others)
+            {
+                std::string_view other_truncated = other.substr(0, end);
+
+                if (first.rfind(other_truncated, 0) != 0)
+                {
+                    one_is_not_matching = true;
+                    break;
+                }
+            }
+
+            if (one_is_not_matching)
+            {
+                --end;
+                break;
+            }
+        }
+
+        if (end == 0)
+            return (std::nullopt);
+
+        return (first.substr(0, end));
+    }
+
     Result complete(std::string &line, bool bell_rang)
     {
         std::set<std::string, string_comparator> candidates;
@@ -81,32 +121,37 @@ namespace autocompletion
             return (Result::NONE);
         else if (candidates.size() == 1)
         {
-            commit(line, *candidates.begin());
+            commit(line, *candidates.begin(), false);
             return (Result::FOUND);
         }
-        else
+
+        auto shared_prefix = find_shared_prefix(candidates);
+        if (shared_prefix.has_value())
         {
-            if (bell_rang)
+            commit(line, shared_prefix.value(), true);
+            return (Result::FOUND);
+        }
+
+        if (bell_rang)
+        {
+            std::cout << std::endl;
+
+            size_t index = 0;
+            for (const auto &candidate : candidates)
             {
-                std::cout << std::endl;
+                if (index++ != 0)
+                    std::cout << "  ";
 
-                size_t index = 0;
-                for (const auto &candidate : candidates)
-                {
-                    if (index++ != 0)
-                        std::cout << "  ";
-
-                    std::cout << line;
-                    std::cout << candidate;
-                }
-
-                std::cout << std::endl;
-
-                prompt();
-                std::cout << line << std::flush;
+                std::cout << line;
+                std::cout << candidate;
             }
 
-            return (Result::MORE);
+            std::cout << std::endl;
+
+            prompt();
+            std::cout << line << std::flush;
         }
+
+        return (Result::MORE);
     }
 }
